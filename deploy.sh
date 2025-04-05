2fd414eed13f55a89ccc794705046fa9a08d67cb
NK <112756092+Narenkarthik2004@users.noreply.github.com>
2025-04-05 09:26:38 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+echo "[DEPLOY] Running build script..."
 chmod +x build.sh
 ./build.sh
 
+echo "[DEPLOY] Building Docker image..."
 docker build -t naren1023/project:latest .
+
+echo "[DEPLOY] Logging in to Docker Hub..."
 docker login -u naren1023 -p Naren@2004
+
+echo "[DEPLOY] Pushing image to Docker Hub..."
 docker push naren1023/project:latest
